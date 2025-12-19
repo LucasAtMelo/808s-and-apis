d37e967076331ccb6a8f6063e7f8916465d4a44e
Lucas Melo <164279989+LucasAtMelo@users.noreply.github.com>
2025-12-19 11:39:37 -0300

    add email for contact in openapi

--- app/urls.py
+++ app/urls.py
@@ -7,11 +7,11 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Music Catalog API",
+        title="808's and API's",
         default_version='v1',
         description="Documentação da API de Catálogo de Músicas e Reviews",
         terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="seu@email.com"),
+        contact=openapi.Contact(email="lucascript0101@gmail.com"),
         license=openapi.License(name="BSD License"),
     ),
     public=True,
